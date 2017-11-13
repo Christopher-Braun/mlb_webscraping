1ea6bd8af1079236b2c30b68eab4983f3e4b9dca
mrcrbraun <mrcrbraun@gmail.com>
2017-11-13 02:29:28 -0600

    Ready for deployment to heroku.

--- baseball/settings.py
+++ baseball/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-	'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
 	# Third party apps
@@ -50,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
